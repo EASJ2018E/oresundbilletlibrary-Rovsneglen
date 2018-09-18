f1a6e1fce11ba1a604c28b89af7562672f5d2315
Jesper Tim Damgaard Krøyer <jesp9030@edu.easj.dk>
2018-09-19 00:07:41 +0200

    Done Done

---
+++ BilØre.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using billetlibrary_Rovsneglen;
+namespace oresundbilletlibrary_Rovsneglen
+{
+    public class BilØre: Bil
+    {
+        public BilØre()
+        {
+            
+        }
+
+        public override double Pris(bool Brobizz)
+        {
+            double price = 410;
+            if (Brobizz)
+                price = 161;
+
+            return price;
+
+        }
+
+        public override string køretøj()
+        {
+            return "Øresund Bil";
+        }
+
+    }
+}
---
+++ MCØre.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using billetlibrary_Rovsneglen;
+
+namespace oresundbilletlibrary_Rovsneglen
+{
+    class MCØre : MC
+    {
+        public MCØre()
+        {
+            
+        }
+
+        public override double Pris(bool Brobizz)
+        {
+            double price = 125;
+            if (Brobizz)
+                price = 73;
+
+            return price;
+        }
+
+        public override string køretøj()
+        {
+            return "Øresund MC";
+        }
+    }
+}
